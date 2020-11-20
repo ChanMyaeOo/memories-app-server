45c1909b5903aa86dd71d05760e85348ca3b0d6d
ChanMyaeOo <chanmyaeoo1221gg@gmail.com>
2020-11-20 12:28:08 +0630

    added .env file

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import postRoutes from './routes/posts.js';
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config();
 
 
 app.use(bodyParser.json({ limit: "30mb", extended: true}));
@@ -12,8 +14,9 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors());
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://admin:admin12345@cluster0.xjtfu.mongodb.net/<dbname>?retryWrites=true&w=majority';
-const PORT = process.env.PORT || 5000;
+// const CONNECTION_URL = 'mongodb+srv://admin:admin12345@cluster0.xjtfu.mongodb.net/<dbname>?retryWrites=true&w=majority';
+const PORT = process.env.PORT;
+const CONNECTION_URL = process.env.CONNECTION_URL;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`)))
