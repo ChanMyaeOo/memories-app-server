ba729280d7849502c765d6a1bf06ab9a488b4e41
ChanMyaeOo <chanmyaeoo1221gg@gmail.com>
2020-11-20 12:30:13 +0630

    added .env to .gitignore

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors());
 app.use('/posts', postRoutes);
 
-// const CONNECTION_URL = 'mongodb+srv://admin:admin12345@cluster0.xjtfu.mongodb.net/<dbname>?retryWrites=true&w=majority';
 const PORT = process.env.PORT;
 const CONNECTION_URL = process.env.CONNECTION_URL;
 
